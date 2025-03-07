dd1dc8e01585f42e7d6730a67805ea2b9d04cfaf
tuan <tuantongthai@gmail.com>
2025-03-07 22:57:27 +0700

    update

--- src/js/main.js
+++ src/js/main.js
@@ -30,3 +30,32 @@ $(document).ready(function(){
 })
 
 
+const swiperThumbs = new Swiper('.swiper--thumbs', {
+    speed: 400,
+    slidesPerView: 3,
+    spaceBetween: 12,
+    loop: false,
+});
+const swiper = new Swiper('.swiper--large', {
+    speed: 400,
+    slidesPerView: 1,
+    spaceBetween: 12,
+    loop: false,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+        swiper: swiperThumbs
+    }
+});
+
+
+$('.product-info__detail-icons').click(function(){
+    $(this).next('.product-info__detail-text').slideToggle();
+    $(this).parent('.product-info__detail-collapsible').toggleClass('product-info__detail-collapsible--active');
+})
